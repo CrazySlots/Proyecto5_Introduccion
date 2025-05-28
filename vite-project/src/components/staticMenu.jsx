6963d5455b18dfabdcfe9295fc0d725a56fdab91
Diego Lopez <diego131322@gmail.com>
2025-05-27 21:35:16 -0600

    Adding some changes before adding project to cloud service

--- vite-project/src/components/staticMenu.jsx
+++ vite-project/src/components/staticMenu.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./staticMenu.css";
 import logo from "../assets/logo.png"
+import foto from "../assets/foto.jpg"
 const TopNav=()=>{
 
     return(
@@ -16,41 +17,38 @@ const TopNav=()=>{
             <a href="#contact">Contacto</a>
         </div>
 
-        <div className="whoAreWe" id="#whoAreWe">
+        <div className="whoAreWe" id="whoAreWe">
             
             <h2>Quienes somos</h2>
-            <figure>
-                <img src="" alt="" />
-                <legend>Soy un estudiante de Ingenieria en sistemas con animo de aprender nuevas herramientas. Listo para superar los retos
-                    que presente la carrera. 
+            <img className="foto" src={foto} alt="" />
+            <div className="text-hover-image">Soy un estudiante de Ingenieria en sistemas con animo de aprender nuevas herramientas. Listo para superar los retos
+            que presente la carrera. </div>
 
-                </legend>
-            </figure>
         </div>
-        <div className="products" id="#producst">
+        <div className="products" id="products">
             <h2>Productos</h2>
         </div>
-        <div className="about" id="#about">
+        <div className="about" id="about">
             <h2>Acerca de</h2>
-            <div className="mission" id="#mision">
+            <div className="mission" id="mision">
                 <h3> Misión</h3>
                 <p>Brindar calzado que represente tu forma de ser y te ayude a expresar quien eres. Ser una marca que se diferencie por opciones variadas y personalizadas utilizando subproductos que de otra forma se desperdiciarian.
                 El calzado mas que una necesidad, es una expresion del alma.</p>
             </div>
-            <div className="vision" id="#vision">
+            <div className="vision" id="vision">
                 <h3>Visión</h3>
                 <p>Ser marca #1 en calzado de cuero y ser reconocida por la personalizacion y expresion de tu ser por medio de nuestros productos.
 
                 </p>
             </div> 
         </div>
-        <div className="vendors" id="#vendors">
+        <div className="vendors" id="vendors">
             <h2>Proveedores</h2>
         </div>
-        <div className="clients" id="#clients">
+        <div className="clients" id="clients">
             <h2>Clientes</h2>
         </div>
-        <div className="contact" id="#contact"> 
+        <div className="contact" id="contact"> 
             <h2>Contacto</h2>
             <ul>
                 <li>Direccion:</li>
